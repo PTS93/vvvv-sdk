281e6a0c34f88fffa4210869fe4e2f2e9edc27a7
Timon Skerutsch <skerutsch@meso.net>
2014-02-14 16:42:40 +0100

    Moved project to meso-plugindevelopment

---
+++ vvvv45/addonpack/src/nodes/plugins/meso-plugindevelopment/MGC3130-Sabrewing/MGC3130.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGC3130_Sabrewing
+{
+    public class MGC3130
+    {
+    }
+}
